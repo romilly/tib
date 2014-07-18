eb9de9a238e9c1f26b225aa26be35c1fe832983f
Romilly Cocking <romilly.cocking@gmail.com>
2014-07-18 18:16:56 +0100

    before push

---
+++ plan/TIB.mm
@@ -0,0 +1,57 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1402247131260" ID="ID_433098218" LINK="../../../Dropbox/tiddly/ActiveProjects.mm" MODIFIED="1402247371138">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      The Intelligent
+    </p>
+    <p style="text-align: center">
+      Breadboard
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#37a3cb" CREATED="1266488217621" ID="Freemind_Link_1242386705" MODIFIED="1266488568255" POSITION="right" TEXT="Goal"/>
+<node COLOR="#373dcb" CREATED="1266488212530" ID="_" MODIFIED="1266488546784" POSITION="right" TEXT="Plan">
+<edge COLOR="#808080"/>
+</node>
+<node COLOR="#de5613" CREATED="1266488223477" ID="Freemind_Link_857865306" MODIFIED="1266488636684" POSITION="right" TEXT="Tasks"/>
+<node COLOR="#fb1802" CREATED="1266488259116" ID="Freemind_Link_1153466861" MODIFIED="1266488895851" POSITION="left" TEXT="Review">
+<icon BUILTIN="xmag"/>
+<node BACKGROUND_COLOR="#006666" COLOR="#ffcc00" CREATED="1266488965666" ID="Freemind_Link_1466796877" MODIFIED="1296303686570" TEXT="WW">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1266488980018" ID="Freemind_Link_1910608691" MODIFIED="1296303689746" TEXT="WDNW">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node COLOR="#00cc00" CREATED="1266488984697" ID="Freemind_Link_1294877740" MODIFIED="1296303577864" TEXT="WDD">
+<icon BUILTIN="licq"/>
+</node>
+<node COLOR="#00cccc" CREATED="1266489024048" ID="Freemind_Link_1003502265" MODIFIED="1296303570801" TEXT="Questions">
+<icon BUILTIN="help"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#ff0000" CREATED="1266489032944" ID="Freemind_Link_19217072" MODIFIED="1296303119998" TEXT="Surprises">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#cc0000" CREATED="1266489043119" ID="Freemind_Link_1706517660" MODIFIED="1296303692921" TEXT="Feelings">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node COLOR="#36d12a" CREATED="1266488273147" ID="Freemind_Link_1517433488" MODIFIED="1266488921179" TEXT="Ideas">
+<icon BUILTIN="idea"/>
+</node>
+<node COLOR="#9679df" CREATED="1266488793745" ID="Freemind_Link_1954142386" MODIFIED="1266488959040" TEXT="Fridge">
+<icon BUILTIN="desktop_new"/>
+</node>
+</node>
+<node COLOR="#27444b" CREATED="1266488248876" ID="Freemind_Link_813012694" MODIFIED="1266488728945" POSITION="left" TEXT="Record">
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#33ff33" CREATED="1273319832507" ID="Freemind_Link_1594441695" MODIFIED="1296303609303" POSITION="left" TEXT="Resources">
+<icon BUILTIN="password"/>
+</node>
+</node>
+</map>
